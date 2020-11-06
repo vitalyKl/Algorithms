4d5a9e945a253161090a3a3d7c715c17b2939350
Vitaly Kalinin <vital5991@gmail.com>
2020-11-06 11:51:48 +0300

    Добавлен рассчёт ИМТ

---
+++ Algorithms/HW1/BMI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Algorithms.HW1
+{
+    public static class BMI
+    {
+        public static void CalculateBMI()
+        {
+            Console.WriteLine("Введите массу:");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите рост:");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            double output = weight / Math.Pow(height, 2);
+
+            Console.WriteLine($"ИМТ = {output}");
+        }
+    }
+}
